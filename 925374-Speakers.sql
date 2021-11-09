69f84bebd0c9560da48a5017c038feb290070729
Peter Aandahl <peter.aandahl@axis.com>
2021-11-09 14:41:08 +0000

    Update query `Speakers`

--- 925374-Speakers.sql
+++ 925374-Speakers.sql
@@ -2,5 +2,5 @@ select l.language, c.population*l.percentage/100 as speakers from country_langua
 join country c
 on l.country_code = c.code
 group by 1
-order by 2,;
+order by 2;
 -- This is a comment
