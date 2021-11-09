626e3f474c1809148c6e2355bae0f4df99bf6162
Peter Aandahl <peter.aandahl@axis.com>
2021-11-09 14:46:29 +0000

    Update query `Speakers`

--- 925374-Speakers.sql
+++ 925374-Speakers.sql
@@ -1,6 +1,6 @@
 -- What are the top languages in the world
-select language, c.population*l.percentage/100 as speakers from country_language l
+select l.language, c.population*l.percentage/100 as speakers from country_language l
 join country c
 on c.code = l.country_code
-group by language l
+group by l.language
 order by population;
